c2e9b22fc9a30d2a59c1674cc007becf30633847
250541022 <250541022@firat.edu.tr>
2025-10-23 15:37:26 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,14 @@
+public class AsciiArt_E {
+
+    public static void main(String[] args) {
+        System.out.println("E harfi:");
+        
+        System.out.println("*********");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*********");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*********");
+    }
+}
