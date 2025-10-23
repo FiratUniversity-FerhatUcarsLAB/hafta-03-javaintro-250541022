f44a3b45594d5ef2e3a325c0e2981a25bf1a54ab
250541022 <250541022@firat.edu.tr>
2025-10-23 15:25:48 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,11 @@
+public class ElifAsciiSimple {
+    public static void main(String[] args) {
+        System.out.println("*****   *      *****  *****");
+        System.out.println("*      * *     *      *    ");
+        System.out.println("*     *   *    *      *    ");
+        System.out.println("***** *****    *****  *****");
+        System.out.println("*    *     *   *      *    ");
+        System.out.println("*   *       *  *      *    ");
+        System.out.println("*****         *****  *    ");
+    }
+}
